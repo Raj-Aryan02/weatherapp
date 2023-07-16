23f03df1b24a22be2fc7370152453f5728ced2c8
Raj-Aryan02 <115422915+Raj-Aryan02@users.noreply.github.com>
2023-07-16 13:54:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,13 +17,19 @@ let weather = {
         const { speed } = data.wind;
         console.log(name, icon, description, temp, humidity, speed)
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+
+
+        document.querySelector(".icon").src ="http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+        // document.querySelector(".icon").src = "img/" + icon + ".svg"
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading")
         document.body.style.backgroundImage = "url('http://source.unsplash.com/1600x900/?" + name + "')"
+
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
