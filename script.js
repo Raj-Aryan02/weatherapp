f585f07891f09d35a761a5c8cb59d3dd2832d47d
Raj-Aryan02 <115422915+Raj-Aryan02@users.noreply.github.com>
2023-07-16 17:17:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ let weather = {
             + "&units=metric&appid="
             + this.apiKey
         )
-            .then((response) => {
-                if (!response.ok) {
-                    alert("Enter valid location");
-                    throw new Error("Enter valid location");
-                }
-                return response.json();
-            })
+        .then((response) => {
+            if (!response.ok) {
+                alert("Enter valid location");
+                throw new Error("Enter valid location");
+            }
+            return response.json();
+        })
             .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
@@ -25,33 +25,59 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name;
 
 
-
-        // document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
         // document.querySelector(".icon").src = "img/" + icon + ".svg"
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading")
-        document.body.style.backgroundImage = "url('http://source.unsplash.com/1600x900/?" + name + "')"
+        document.body.style.backgroundImage = "url('http://source.unsplash.com/1600x900/?" + name + "')";
 
- let iconUrl;
+        const iconElement = document.querySelector(".icon");
+        iconElement.className = "icon";
+
         if (description.includes("clear")) {
-            iconUrl = "http://openweathermap.org/img/wn/01d.png"; // Clear sky
+            iconElement.src = "img/clear.svg"; // Clear sky
+        } else if (description.includes("broken clouds")) {
+            iconElement.src = "img/broken clouds.svg"; // Broken clouds
+        } else if (description.includes("scattered clouds")) {
+            iconElement.src = "img/scattered clouds.svg"; // Scattered clouds
+        } else if (description.includes("few clouds")) {
+            iconElement.src = "img/few clouds.svg"; // Few Clouds
         } else if (description.includes("clouds")) {
-            iconUrl = "http://openweathermap.org/img/wn/02d.png"; // Cloudy
+            iconElement.src = "img/clouds.svg"; // Cloudy   
+        } else if (description.includes("light rain")) {
+            iconElement.src = "img/light-rain.svg"; // Light Rain
+        } else if (description.includes("moderate rain")) {
+            iconElement.src = "img/moderate rain.svg"; // Moderate Rain
+        } else if (description.includes("heavy rain")) {
+            iconElement.src = "img/heavy rain.svg"; // Heavy Rain
+        } else if (description.includes("shower rain")) {
+            iconElement.src = "img/shower rain.svg"; // Shower Rain
         } else if (description.includes("rain")) {
-            iconUrl = "http://openweathermap.org/img/wn/10d.png"; // Rain
-        } else if (description.includes("snow")) {
-            iconUrl = "http://openweathermap.org/img/wn/13d.png"; // Snow
-        }else if (description.includes("haze")) {
-            iconUrl = "img/haze.svg"; //haze
+            iconElement.src = "img/rain.svg"; // Rain
+        }else if (description.includes("light snow")) {
+            iconElement.src = "img/light snow.svg"; // Light snow
+        }else if (description.includes("heavy snow")) {
+            iconElement.src = "img/heavy snow.svg"; // Heavy Snow
+        }
+         else if (description.includes("snow")) {
+            iconElement.src = "img/snow.svg"; // Snow
+        } else if (description.includes("haze")) {
+            iconElement.src = "img/haze.png" //Haze
+        } else if (description.includes("mist")) {
+            iconElement.src = "img/mist.png" //Mist
+        } else if (description.includes("thunderstorm")) {
+            iconElement.src = "img/thunder.svg"; // Thunderstorm  
+        } else if (description.includes("drizzle")) {
+            iconElement.src = "img/drizzle.svg"; // Drizzle
         }
         else {
-            iconUrl = "http://openweathermap.org/img/wn/01d.png"; // Default icon
+            iconElement.src = "img/clear.svg"; // Default icon
         }
-        
+
+        // document.querySelector(".icon").src = iconUrl;
+
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
